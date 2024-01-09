f98b02cd87af34e500d277a1fa2779a7061cb3cb
Anjan Vikas <anjanvikas2001@gmail.com>
2024-01-09 20:09:16 +0530

    Update linqquery.cs

--- linqquery.cs
+++ linqquery.cs
@@ -1,5 +1,3 @@
-(from row1 in dt1.AsEnumerable()
-                         join row2 in dt2.AsEnumerable() on row1.Field<string>("Column1") equals row2.Field<string>("Column2") into gj
-                         from subRow in gj.DefaultIfEmpty()
-                         where subRow == null
-                         select row1.Field<string>("Column1")).ToList();
+var itemsNotInColumn2 = dt.AsEnumerable()
+    .Select(row => row.Field<string>("Column1"))
+    .Except(dt.AsEnumerable().Select(row => row.Field<string>("Column2")));
